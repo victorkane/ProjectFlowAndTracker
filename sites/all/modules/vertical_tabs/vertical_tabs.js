3af8b1a3ede315f211fe2af1b3f2a3957b65a5b8
Victor Kane <victorkane@gmail.com>
2010-09-18 12:26:50 -0400

    upgraded token and vertical_tabls

--- sites/all/modules/vertical_tabs/vertical_tabs.js
+++ sites/all/modules/vertical_tabs/vertical_tabs.js
@@ -1,4 +1,4 @@
-// $Id: vertical_tabs.js,v 1.3.2.18 2009/12/04 05:18:57 davereid Exp $
+// $Id: vertical_tabs.js,v 1.3.2.19 2010/02/03 18:24:42 davereid Exp $
 
 Drupal.verticalTabs = Drupal.verticalTabs || {};
 Drupal.settings.verticalTabs = Drupal.settings.verticalTabs || {};
@@ -51,6 +51,13 @@ Drupal.behaviors.verticalTabs = function() {
     });
 
     $('div.vertical-tabs').html(ul).append(panes);
+
+    // Add an error class to any fieldsets with errors in them.
+    $('fieldset.vertical-tabs-pane').each(function(i){
+      if ($(this).find('div.form-item .error').size()) {
+        $('li.vertical-tab-button').eq(i).addClass('error');
+      }
+    })
 
     // Activate the first tab.
     $('fieldset.vertical-tabs-pane').hide();
